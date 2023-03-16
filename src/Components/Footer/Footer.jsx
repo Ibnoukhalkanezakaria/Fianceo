a1501ebc9ce1612357f0977d8f60a4dd1fd8eae7
Ibnoukhalkanezakaria <Ibnoukhalkanezakaria@gmail.com>
2023-03-16 17:17:14 +0100

    new

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,120 +4,126 @@ import Logo from "../../assets/img-logo.svg";
 import { TfiFacebook, TfiLinkedin } from "react-icons/tfi";
 import { TiSocialTwitter } from "react-icons/ti";
 import { RiInstagramFill } from "react-icons/ri";
+import { SlEnvolopeLetter } from "react-icons/sl";
 
 const Footer = () => {
   return (
-    <div className="Wrapper pt-[100px] bg-color7">
-      <div className="logo">
-        <img src={Logo} alt="Logo" aria-label="Logo" />
-      </div>
-      <div className="flex flex-row py-10">
-        <div className="basis-1/4">
-          <h3 className="color5 fw-700 f5 pb-6">Product</h3>
-          <ul role="list" aria-label="Nav-Links">
-            <li className="pb-5">
-              <Link to="/" className="color5">
-                Home
-              </Link>
-            </li>
-            <li className="pb-5">
-              <Link to="/integeation" className="color5">
-                Integeation
-              </Link>
-            </li>
-            <li>
-              <Link to="/pricing" className="color5">
-                Pricing
-              </Link>
-            </li>
-          </ul>
+    <div className="bg-color7">
+      <div className="Wrapper pt-8">
+        <div className="logo">
+          <img src={Logo} alt="Logo" aria-label="Logo" />
         </div>
-        <div className="basis-1/4">
-          <h3 className="color5 fw-700 f5 pb-6">Company</h3>
-          <ul role="list" aria-label="Nav-Links">
-            <li className="pb-5">
-              <Link to="/" className="color5">
-                About us
-              </Link>
-            </li>
-            <li className="pb-5">
-              <Link to="/integeation" className="color5">
-                Blogs
-              </Link>
-            </li>
-            <li className="pb-5">
-              <Link to="/pricing" className="color5">
-                Sign In
-              </Link>
-            </li>
-            <li className="pb-5">
-              <Link to="/pricing" className="color5">
-                Sign Up
-              </Link>
-            </li>
-          </ul>
+        <div className="flex flex-wrap md:flex-row gap-8 justify-start py-12 ">
+          <div className="basis-1/4 order-footer-3 ">
+            <h3 className="color5 fw-700 f5">Product</h3>
+            <ul role="list" aria-label="Nav-Links">
+              <li className="pt-4">
+                <Link to="/" className="color5">
+                  Home
+                </Link>
+              </li>
+              <li className="pt-4">
+                <Link to="/integeation" className="color5">
+                  Integeation
+                </Link>
+              </li>
+              <li className="pt-4">
+                <Link to="/pricing" className="color5">
+                  Pricing
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="basis-1/4 order-footer-2 ">
+            <h3 className="color5 fw-700 f5 ">Company</h3>
+            <ul role="list" aria-label="Nav-Links">
+              <li className="pt-4">
+                <Link to="/" className="color5">
+                  About us
+                </Link>
+              </li>
+              <li className="pt-4">
+                <Link to="/integeation" className="color5">
+                  Blogs
+                </Link>
+              </li>
+              <li className="pt-4">
+                <Link to="/pricing" className="color5">
+                  Sign In
+                </Link>
+              </li>
+              <li className="pt-4">
+                <Link to="/pricing" className="color5">
+                  Sign Up
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="basis-1/1 flex-one">
+            <h3 className="color5 fw-700 sm:text-xl text-md pb-4 flex gap-2 ">
+              <SlEnvolopeLetter /> Join our newsletter
+            </h3>
+            <form className="flex flex-wrap gap-2">
+              <input
+                className="p-4 sm:w-[300px] w-full focus:outline-none "
+                type="email"
+                placeholder="example@gmail.com"
+                required
+              />
+              <input
+                type="submit"
+                value="Subscribe Now"
+                className="sm:w-[150px] w-full f8 bg-color3 color7 py-5 px-4 text-center cursor-pointer hover:opacity-80"
+              />
+            </form>
+            <ul className="list-disc pl-5">
+              <li className="max-w-[500px] leading-[25px] ">
+                <span className="text-[#00000080] pt-4  block max-w-[400px]">
+                  Will send you weekly updates for your better finance
+                  management
+                </span>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="basis-1/2">
-          <h3 className="color5 fw-700 f5 pb-6">Join our newsletter</h3>
-          <form className="flex">
-            <input
-              className="px-4 focus:outline-none"
-              type="email"
-              placeholder="example@gmail.com"
-              required
-            />
-            <input
-              type="submit"
-              value="Subscribe Now"
-              className="w-[150px] f8 bg-color3 color7 py-5 px-4 text-center cursor-pointer hover:opacity-80"
-            />
-          </form>
-          <ul className="list-disc pt-6 pl-[3%] ">
-            <li className="max-w-[500px] leading-[25px]">
-              <span className="text-[#00000080] ">
-                Will send you weekly updates for your better finance management
-              </span>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <hr className="mt-5" />
-      <div className="py-8 flex flex-row justify-between">
-        <div>
-          <p className="fw-400 ">
-            © Ibnoukhalkane 2023 Fianceo. Design by Webflow, checkout the
-            repository
-            <a
-              target="_blank"
-              href="https://github.com/Ibnoukhalkanezakaria/Fianceo"
-              className="color3
+        <hr />
+        <div className="flex flex-row flex-wrap gap-6 justify-between py-8">
+          <div>
+            <p className="fw-400 ">
+              © Ibnoukhalkane 2023 Fianceo. Design by Webflow, checkout the
+              repository
+              <a
+                target="_blank"
+                href="https://github.com/Ibnoukhalkanezakaria/Fianceo"
+                className="color3
               pl-2"
-            >
-              here
-            </a>
-          </p>
-        </div>
-        <div className="flex gap-10 items-center">
-          <span className="cursor-pointer">
-            <a target="_blank" href="https://www.facebook.com/">
-              <TfiFacebook />
-            </a>
-          </span>
-          <span className="cursor-pointer">
-            <a target="_blank" href="https://twitter.com/home">
-              <TiSocialTwitter />
-            </a>
-          </span>
-          <span className="cursor-pointer">
-            <a target="_blank" href="https://www.linkedin.com/">
-              <TfiLinkedin />
-            </a>
-          </span>
-          <span className="cursor-pointer">
-            <a target="_blank" href="https://www.instagram.com/">
-              <RiInstagramFill />
-            </a>
-          </span>
+              >
+                here
+              </a>
+            </p>
+          </div>
+          <div className="flex gap-10 items-center">
+            <span className="cursor-pointer">
+              <a target="_blank" href="https://www.facebook.com/">
+                <TfiFacebook />
+              </a>
+            </span>
+            <span className="cursor-pointer">
+              <a target="_blank" href="https://twitter.com/home">
+                <TiSocialTwitter />
+              </a>
+            </span>
+            <span className="cursor-pointer">
+              <a target="_blank" href="https://www.linkedin.com/">
+                <TfiLinkedin />
+              </a>
+            </span>
+            <span className="cursor-pointer">
+              <a target="_blank" href="https://www.instagram.com/">
+                <RiInstagramFill />
+              </a>
+            </span>
+          </div>
         </div>
       </div>
     </div>
