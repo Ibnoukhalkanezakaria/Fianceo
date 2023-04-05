cc949a9ef1bb4ccb855f8e626cd168300a2413c5
Ibnoukhalkanezakaria <Ibnoukhalkanezakaria@gmail.com>
2023-04-05 00:07:20 +0000

    new

--- src/Components/Slider/SliderCnp.jsx
+++ src/Components/Slider/SliderCnp.jsx
@@ -31,7 +31,7 @@ const SliderCnp = () => {
   };
 
   return (
-    <div className=" mb-40">
+    <div className="md:mb-40 mb-28">
       <div className="">
         <div className="relative ">
           <div>
