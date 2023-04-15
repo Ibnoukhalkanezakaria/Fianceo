b86ca29542ae47ba41457bbf793f9fd9a8457f71
Ibnoukhalkanezakaria <Ibnoukhalkanezakaria@gmail.com>
2023-04-15 10:28:10 +0000

    new

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div className="text-center p-20">
+      <h1 className="text-4xl">404</h1>
+      <span>
+        Go Back{" "}
+        <Link to="/" className="text-blue-500">
+          Home
+        </Link>
+      </span>
+    </div>
+  );
 };
 
 export default Error;
